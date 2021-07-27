645635934d278fef49ce185f800dd57cc965c602
themosesnyambu <nyamosekitando@gmail.com>
2021-07-27 09:58:43 +0300

    fixed line break linting issue LF to CLRF

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT  = process.env.PORT || 3000;
-const app:Express = express();
+const PORT = process.env.PORT || 3000;
+const app: Express = express();
 
 app.use(helmet());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res:Response) => {
-    res.send('Well done!');
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send('Well done!');
+});
 
 app.listen(PORT, () => {
-    console.log('The application is listening on port 3000!');
-})
+  // eslint-disable-next-line no-console
+  console.log('The application is listening on port 3000!');
+});
